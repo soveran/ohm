89d3a39ac9fee7ed5bd97ff7e1b824dd394b56a1
Damian Janowski <damian.janowski@gmail.com>
2009-05-16 18:20:36 -0300

    Separated save/create, moved to a namespace.

--- lib/ohm.rb
+++ lib/ohm.rb
@@ -1,71 +1,91 @@
 require "rubygems"
 require "redis"
 
-class Model
-  @@attributes = Hash.new { |h,k| h[k] = [] }
+module Ohm
+  class Model
+    ModelIsNew = Class.new(StandardError)
 
-  attr_accessor :id
+    @@attributes = Hash.new { |h,k| h[k] = [] }
 
-  def self.attribute(name)
-    attr_accessor(name)
-    attributes << name
-  end
+    attr_accessor :id
 
-  def self.[](id)
-    if result = $redis[key(self, id)]
-      model = new
-      model.id = id
+    def self.attribute(name)
+      attr_writer(name)
+      attr_lazy_reader(name)
+      attributes << name
+    end
 
-      attributes.each do |att|
-        model.send("#{att}=", $redis[key(self, id, att)])
+    def self.attr_lazy_reader(name)
+      class_eval <<-EOS
+        def #{name}
+          @#{name} ||= db[key(#{name.inspect})]
+        end
+      EOS
+    end
+
+    def self.[](id)
+      if db[key(self, id)]
+        model = new
+        model.id = id
+        model
+      end
+    end
+
+    def self.all
+      db.set_members(key(self)).map do |id|
+        self[id]
+      end
+    end
+
+    def self.attributes
+      @@attributes[self]
+    end
+
+    def self.next_id
+      db.incr(key(self, "id"))
+    end
+
+    def new?
+      ! self.id
+    end
+
+    def create
+      self.id = self.class.next_id
+      db.set_add(self.class.key(self.class), self.id)
+      db[key] = true
+      save
+    end
+
+    def save
+      ensure_model_exists
+
+      self.class.attributes.each do |att|
+        db[key(att)] = send(att)
       end
 
-      model
-    end
-  end
-
-  def self.all
-    $redis.set_members(key(self)).map do |id|
-      self[id]
-    end
-  end
-
-  def self.attributes
-    @@attributes[self]
-  end
-
-  def self.next_id
-    $redis.incr(key(self, "id"))
-  end
-
-  # This check shouldn't be necessary if we add the
-  # create method.
-  def new?
-    ! self.id
-  end
-
-  # What about having a create and a save method?
-  # The save method would raise an error if the id is nil.
-  # The create method would assign an id and then call save.
-  def save
-    if new?
-      self.id = self.class.next_id
-      $redis.set_add(self.class.key(self.class), self.id)
-      $redis[key] = true
+      self
     end
 
-    self.class.attributes.each do |att|
-      $redis[key(att)] = send(att)
+  private
+
+    def ensure_model_exists
+      raise ModelIsNew if new?
     end
-  end
 
-private
+    def key(*args)
+      self.class.key([self.class, id] + args)
+    end
 
-  def key(*args)
-    self.class.key([self.class, id] + args)
-  end
+    def self.key(*args)
+      args.join(":")
+    end
 
-  def self.key(*args)
-    args.join(":")
+    def db
+      self.class.db
+    end
+
+    def self.db
+      $redis
+    end
   end
 end
--- test/model_test.rb
+++ test/model_test.rb
@@ -6,11 +6,11 @@ require File.dirname(__FILE__) + "/../lib/ohm"
 $redis = Redis.new(:port => 6381)
 $redis.flush_db
 
-class Event < Model
+class Event < Ohm::Model
   attribute :name
 end
 
-class User < Model
+class User < Ohm::Model
   attribute :email
 end
 
@@ -68,12 +68,31 @@ class TestRedis < Test::Unit::TestCase
   context "creating" do
     should "increment the ID" do
       event1 = Event.new
-      event1.save
+      event1.create
 
       event2 = Event.new
-      event2.save
+      event2.create
 
       assert_equal event1.id + 1, event2.id
+    end
+  end
+
+  context "saving" do
+    should "not save a new model" do
+      assert_raise Ohm::Model::ModelIsNew do
+        Event.new.save
+      end
+    end
+
+    should "save if the model was previously created" do
+      event = Event.new
+      event.name = "Lorem ipsum"
+      event.create
+
+      event.name = "Lorem"
+      event.save
+
+      assert_equal "Lorem", Event[event.id].name
     end
   end
 
@@ -81,16 +100,31 @@ class TestRedis < Test::Unit::TestCase
     should "find all" do
       event1 = Event.new
       event1.name = "Ruby Meetup"
-      event1.save
+      event1.create
 
       event2 = Event.new
       event2.name = "Ruby Tuesday"
-      event2.save
+      event2.create
 
       all = Event.all
 
       assert all.detect {|e| e.name == "Ruby Meetup" }
       assert all.detect {|e| e.name == "Ruby Tuesday" }
+    end
+  end
+
+  context "internals" do
+    setup do
+      event = Event.new
+      event.name = "Ruby Tuesday"
+      @id = event.create.id
+    end
+
+    should "load attributes lazily" do
+      event = Event[@id]
+
+      assert_nil event.send(:instance_variable_get, "@name")
+      assert_equal "Ruby Tuesday", event.name
     end
   end
 end
