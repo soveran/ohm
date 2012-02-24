213f34e3baca51416f7c9b7f4c17f02c08067921
Cyril David <cyx.ucron@gmail.com>
2012-02-24 22:01:55 +0800

    Temporary commit for soveran.

--- lib/ohm.rb
+++ lib/ohm.rb
@@ -5,42 +5,46 @@ require "redis"
 require "nest"
 
 module Ohm
-  def self.redis
-    @redis ||= Redis.connect(options)
-  end
-
-  def self.redis=(redis)
-    @redis = redis
-  end
-
-  def self.connect(options = {})
-    @redis = nil
-    @options = options
-  end
-
-  def self.options
-    @options ||= {}
-  end
-
   class Error < StandardError; end
   class MissingID < Error; end
 
-  class Model
-    def self.db
-      @db ||= (defined?(@options) ? Redis.connect(options) : Ohm.redis)
-    end
-
-    def self.connect(options = {})
-      @db = nil
+  module ConnectionHandling
+    def connect(options = {})
+      self.redis = nil
       @options = options
     end
 
-    def self.options
-      @options
+    def redis
+      threaded[self] ||= Redis.connect(options)
+    end
+
+    def redis=(redis)
+      threaded[self] = redis
+    end
+
+    def threaded
+      Thread.current[:Ohm] ||= {}
+    end
+
+    def options
+      @options || {}
+    end
+  end
+  extend ConnectionHandling
+
+  class Model
+    extend ConnectionHandling
+
+    def self.db
+      options.any? ? redis : Ohm.redis
     end
 
     def self.key
-      @key ||= Nest.new(key, db)
+      @key ||= Nest.new(self.name, db)
+    end
+
+    def self.[](id)
+      new(id: id).load! if id && exists?(id)
     end
 
     def self.exists?(id)
@@ -48,7 +52,78 @@ module Ohm
     end
 
     def self.new_id
-      key[:id].incr.to_s
+      key[:id].incr
+    end
+
+    def self.attributes
+      @attributes ||= []
+    end
+
+    def self.attribute(name, cast = nil)
+      if cast
+        define_method(name) do
+          cast[@_attributes[name]]
+        end
+      else
+        define_method(name) do
+          @_attributes[name]
+        end
+      end
+
+      define_method(:"#{name}=") do |value|
+        @_attributes[name] = value
+      end
+
+      attributes << name unless attributes.include?(name)
+    end
+
+    class Collection < Struct.new(:set, :key, :model)
+      include Enumerable
+
+      def each
+        to_a.each do |e|
+          yield e
+        end
+      end
+
+      def size
+        set.scard
+      end
+
+      def sort(options = {})
+        @sort = options
+        return self
+      end
+
+      def sort_by(att, options = {})
+        sort(options.merge(by: key["*->%s" % att]))
+      end
+
+      def to_a
+        @elements ||= begin
+          ids = members
+
+          arr = model.db.pipelined do
+            ids.each { |id| key[id].hgetall }
+          end
+
+          arr.map.with_index do |atts, idx|
+            model.new(Hash[*atts].update(id: ids[idx]))
+          end
+        end
+      end
+
+      def members
+        if defined?(@sort)
+          set.sort(@sort)
+        else
+          set.smembers
+        end
+      end
+    end
+
+    def self.all
+      Collection.new(key[:all], key, self)
     end
 
     def model
@@ -59,8 +134,57 @@ module Ohm
       model.db
     end
 
+    def key
+      model.key[id]
+    end
+
+    attr_accessor :id
+
+    def initialize(atts = {})
+      @_attributes = {}
+      update_attributes(atts)
+    end
+
+    def update_attributes(atts)
+      atts.each { |att, val| send(:"#{att}=", val) }
+    end
+
+    def load!
+      update_attributes(key.hgetall) unless new?
+
+      return self
+    end
+
+    def new?
+      !@id
+    end
+
+    def save
+      if new?
+        _initialize_id
+        key.hmset(*_flattened_attributes)
+
+        model.key[:all].sadd(id)
+      else
+        key.del
+        key.hmset(*_flattened_attributes)
+      end
+
+      return self
+    end
+
     def _initialize_id
       @id ||= model.new_id
+    end
+
+    def _flattened_attributes
+      ret = []
+
+      @_attributes.each do |att, val|
+        ret.push(att, val)
+      end
+
+      return ret
     end
   end
 end
---
+++ test/bench.rb
@@ -0,0 +1,45 @@
+require File.expand_path("../lib/ohm", File.dirname(__FILE__))
+require "redis/connection/hiredis"
+
+Ohm.redis.flushdb
+
+class User < Ohm::Model
+  attribute :fname
+  attribute :lname
+  attribute :bday
+  attribute :gender
+  attribute :city
+  attribute :state
+  attribute :country
+  attribute :zip
+end
+
+1000.times do |i|
+  user = User.new(fname: "John#{i}",
+                  lname: "Doe#{i}",
+                  bday: Time.now.to_s,
+                  gender: "Male",
+                  city: "Los Angeles",
+                  state: "CA",
+                  country: "US",
+                  zip: "90210").save
+end
+
+require "benchmark"
+
+t1 = Benchmark.realtime do
+  User.all.sort_by(:fname, order: "DESC ALPHA").each do |user|
+  end
+end
+
+t2 = Benchmark.realtime do
+  ids = User.key[:all].smembers
+
+  ids.each do |id|
+    User[id]
+  end
+end
+
+puts t1
+puts t2
+puts t2 / t1
