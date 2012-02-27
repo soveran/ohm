285a7c105cfa878a1ce253c8caa7962f818f3c68
Cyril David <cyx.ucron@gmail.com>
2012-02-28 02:58:11 +0800

    Add lua-save.

---
+++ test/lua-save.rb
@@ -0,0 +1,154 @@
+require File.expand_path("./helper", File.dirname(__FILE__))
+
+def redis
+  Ohm.redis
+end
+
+setup do
+  redis.sadd("User:uniques", "email")
+  redis.sadd("User:indices", "fname")
+  redis.sadd("User:indices", "lname")
+  redis.hset("User:uniques:email", "foo@bar.com", 1)
+
+  Ohm::Lua.new("./test/lua", redis)
+end
+
+test "returns the unique constraint error" do |lua|
+  res = lua.run("save",
+    keys: ["User"],
+    argv: ["email", "foo@bar.com"])
+
+  assert_equal [500, ["email", "not_unique"]], res
+end
+
+test "persists the unique entry properly" do |lua|
+  lua.run("save",
+    keys: ["User"],
+    argv: ["email", "bar@baz.com"])
+
+  assert_equal "1", redis.hget("User:uniques:email", "bar@baz.com")
+end
+
+test "adds the entry to User:all" do |lua|
+  lua.run("save",
+    keys: ["User"],
+    argv: ["email", "bar@baz.com"])
+
+  assert_equal 1, redis.scard("User:all")
+end
+
+
+test "saves the attributes" do |lua|
+  lua.run("save",
+    keys: ["User"],
+    argv: ["email", "bar@baz.com", "fname", "John", "lname", "Doe"])
+
+  assert_equal "bar@baz.com", redis.hget("User:1", "email")
+  assert_equal "John", redis.hget("User:1", "fname")
+  assert_equal "Doe", redis.hget("User:1", "lname")
+end
+
+test "indexes fname / lname" do |lua|
+  lua.run("save",
+    keys: ["User"],
+    argv: ["email", "bar@baz.com", "fname", "John", "lname", "Doe"])
+
+  assert redis.sismember("User:indices:fname:John", 1)
+  assert redis.sismember("User:indices:lname:Doe", 1)
+end
+
+test "unique constraint during update" do |lua|
+  lua.run("save",
+    keys: ["User"],
+    argv: ["email", "bar@baz.com", "fname", "John", "lname", "Doe"])
+
+  res = lua.run("save",
+    keys: ["User", "User:1"],
+    argv: ["email", "bar@baz.com", "fname", "John", "lname", "Doe"])
+
+  assert_equal [200, ["id", "1"]], res
+
+  res = lua.run("save",
+    keys: ["User", "User:1"],
+    argv: ["email", "foo@bar.com", "fname", "Jane", "lname", "Doe"])
+
+  assert_equal [200, ["id", "1"]], res
+end
+
+test "cleanup of existing indices during update" do |lua|
+  lua.run("save",
+    keys: ["User"],
+    argv: ["email", "bar@baz.com", "fname", "John", "lname", "Doe"])
+
+  res = lua.run("save",
+    keys: ["User", "User:1"],
+    argv: ["email", "foo@bar.com", "fname", "Jane", "lname", "Smith"])
+
+  assert ! redis.sismember("User:indices:fname:John", 1)
+  assert ! redis.sismember("User:indices:fname:Doe", 1)
+end
+
+test "cleanup of existing uniques during update" do |lua|
+  lua.run("save",
+    keys: ["User"],
+    argv: ["email", "bar@baz.com", "fname", "John", "lname", "Doe"])
+
+  res = lua.run("save",
+    keys: ["User", "User:1"],
+    argv: ["email", "foo@bar.com", "fname", "Jane", "lname", "Smith"])
+
+  assert_equal nil, redis.hget("User:uniques:email", "bar@baz.com")
+end
+
+__END__
+test "stress test for lua scripting" do |lua|
+  require "benchmark"
+
+  t = Benchmark.measure do
+    threads = 100.times.map do |i|
+      Thread.new do
+        lua.run("save",
+          keys: ["User"],
+          argv: ["email", "foo#{i}@bar.com",
+                 "fname", "Jane#{i}",
+                 "lname", "Smith#{i}"])
+      end
+    end
+
+    threads.each(&:join)
+  end
+
+  puts t
+end
+
+test "stress test for postgres + sequel (as a comparison)" do
+  require "sequel"
+
+  DB = Sequel.connect("postgres://cyx@localhost/postgres")
+  DB[:users].truncate
+  t = Benchmark.measure do
+    threads = 100.times.map do |i|
+      Thread.new do
+        DB[:users].insert(email: "foo#{i}@bar.com",
+                          fname: "John#{i}",
+                          lname: "Doe#{i}")
+      end
+    end
+
+    threads.each(&:join)
+  end
+
+  puts t
+end
+
+## Result for 100 threads:
+#  0.040000   0.020000   0.060000 (  0.066827) - lua script
+#  0.150000   0.180000   0.330000 (  0.259676) - postgres
+#
+## Result for 100 linear executions:
+#
+#  0.010000   0.010000   0.020000 (  0.026081) - lua script
+#  0.010000   0.010000   0.020000 (  0.059540) - postgres
+#
+## It's also important to note that with 1K concurrent threads,
+#  postgres throws a Sequel::PoolTimeout
--- test/lua/save.lua
+++ test/lua/save.lua
@@ -1,13 +1,190 @@
-local key = KEYS[1]
-local all = KEYS[2]
-local id  = string.match(key, "(%w+)$")
+-- COMMAND save
+-- KEYS[1] the namespace, e.g. `User`
+-- KEYS[2] (optional) the actual Hash key e.g. `User:1`
+--         for the case of a new record, we leave this blank.
+--
+-- ARGV[1] the flattened attributes of the model. for uniques and
+--         indices to work, this should include all the indices
+--         and uniques in addition.
+--
+--         Example:
+--
+--           attribute: title, tags
+--           unique: slug
+--           index:  date, title
+--
+--           expected ARGV[1]:
+--           { "title", "...", "tags", "...", "slug", ...", "date", "..." }
+--
+--           We can skip the title for the index since that has already been
+--           specified.
+--
 
-local save = function (atts)
-  redis.call("DEL", key)
-  redis.call("HMSET", key, unpack(atts))
-  redis.call("SADD", all, id)
+local namespace  = KEYS[1]
+local key        = KEYS[2]
+local attributes = ARGV
+local id
+
+if key then
+  -- since there was a key passed, we can extract the ID from the
+  -- key, e.g. `User:1` means an ID of 1.
+  id = string.match(key, "(%w+)$")
 end
 
-save(ARGV)
+local model = {
+  id      = namespace .. ":id",
+  all     = namespace .. ":all",
+  uniques = namespace .. ":uniques",
+  indices = namespace .. ":indices",
+  key     = namespace .. ":%s"
+}
 
-return "OK"
+local meta = {
+  uniques = redis.call("SMEMBERS", model.uniques),
+  indices = redis.call("SMEMBERS", model.indices)
+}
+
+-- Converts a flattened list to a key-value dictionary.
+-- Used to translate the ARGV into a lua table we can easily use.
+local function dict(list)
+  local ret = {}
+
+  for i = 1, #list, 2 do
+    ret[list[i]] = list[i + 1]
+  end
+
+  return ret
+end
+
+-- Converts a table into a flattened list. Used to translate
+-- the table into the final set of parameters we pass to HMSET.
+local function list(table)
+  local atts = {}
+
+  for k, v in pairs(table) do
+    atts[#atts + 1] = k
+    atts[#atts + 1] = v
+  end
+
+  return atts
+end
+
+-- Even before we attempt to save this record, we need to verify
+-- that no unique constraints have been violated. If so, we return
+-- an error code like so:
+--
+--   { 500, { "email", "not_unique" }}
+local function detect_duplicate(table, id)
+  for _, att in ipairs(meta.uniques) do
+    local key = model.uniques .. ":" .. att
+    local existing = redis.call("HGET", key, table[att])
+
+    if existing and existing ~= tostring(id) then return att end
+  end
+end
+
+-- Given the namespace `User` and the unique attribute `email` with a
+-- value of 'foo@bar.com', and an ID of 1, we store that into a HASH
+-- like so:
+--
+-- HSET User:uniques:email foo@bar.com 1
+--
+local function save_uniques(table, id)
+  for _, att in ipairs(meta.uniques) do
+    local key = model.uniques .. ":" .. att
+
+    redis.call("HSET", key, table[att], id)
+  end
+end
+
+-- Used to cleanup existing unique values stored.
+local function delete_uniques(hash, id)
+  for _, att in ipairs(meta.uniques) do
+    local key = model.uniques .. ":" .. att
+    local val = redis.call("HGET", hash, att)
+
+    redis.call("HDEL", key, val, id)
+  end
+end
+
+-- Given the namespace `User` and the unique index `fname` with a
+-- value of 'John', and an ID of 1, we store that into a SET like so
+--
+-- SADD User:indices:fname:John 1
+--
+local function save_indices(table, id)
+  for _, att in ipairs(meta.indices) do
+    local key = model.indices .. ":" .. att .. ":" .. table[att]
+
+    redis.call("SADD", key, id)
+  end
+end
+
+-- This is used to cleanup already existing indices previously saved.
+local function delete_indices(hash, id)
+  for _, att in ipairs(meta.indices) do
+    local val = redis.call("HGET", hash, att)
+
+    if val then
+      local key  = model.indices .. ":" .. att .. ":" .. val
+
+      redis.call("SREM", key, id)
+    end
+  end
+end
+
+-- You can say this is the crux of this script in the sense that
+-- these statements persist all the attributes, indices, and uniques
+-- passed into ARGV.
+local function save(hash, key)
+  redis.call("DEL", key)
+  redis.call("HMSET", key, unpack(list(hash)))
+end
+
+-- In the future, we probably plan to allow users to hook into scripts
+-- on a per model basis. They can tap into `this` and possibly do
+-- some manipulations before persisting it finally.
+local this = dict(attributes)
+
+-- We try to find any existing duplicates for all of the declared
+-- uniques of the model. Ideally you've validated that the entry
+-- is already unique even before it reaches this point, but this also
+-- serves as the last line of defense, similar to constraints in an RDBMS.
+local duplicate = detect_duplicate(this, id)
+
+-- We short-circuit the script if we find any form of unique constraint
+-- violation.
+if duplicate then
+  return { 500, { duplicate, "not_unique" }}
+end
+
+-- This is the great differentiator between a new record and an existing
+-- one: an existing one should have an ID.
+--
+-- Given that this is a new record, we have to do a couple of stuff namely:
+--
+-- 1. Generate a new ID.
+-- 2. Add that generated id to `model.all` (e.g. User:all)
+-- 3. Reflect that change into the local variable `key`, e.g. it now
+--    becomes `User:1`.
+if not id then
+  id = redis.call("INCR", model.id)
+  redis.call("SADD", model.all, id)
+  key = model.key:format(id)
+end
+
+-- Now comes the easy part: We first cleanup both indices and uniques.
+-- It's important that we do this before saving, otherwise the old
+-- values of the uniques and indices will be lost.
+delete_uniques(key, id)
+delete_indices(key, id)
+
+-- Now that we've cleaned up, we can now persist the new attributes.
+save(this, key)
+
+-- In addition, we can now also save the new uniques and indices.
+save_uniques(this, id)
+save_indices(this, id)
+
+-- Happy with a new record, we can now safely return. ^_^
+return { 200, { "id", id }}
