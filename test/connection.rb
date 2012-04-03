078f0a4caac0d4ba87a93c325b3882faa6eec330
Cyril David <cyx.ucron@gmail.com>
2012-04-04 06:31:59 +0800

    Revert to make it work with redis 2.2.2.

--- test/connection.rb
+++ test/connection.rb
@@ -24,7 +24,7 @@ test "connects lazily" do
   begin
     Ohm.redis.get "foo"
   rescue => e
-    assert_equal Redis::CannotConnectError, e.class
+    assert_equal Errno::ECONNREFUSED, e.class
   end
 end
 
@@ -54,7 +54,7 @@ test "supports connecting by URL" do
   begin
     Ohm.redis.get "foo"
   rescue => e
-    assert_equal Redis::CannotConnectError, e.class
+    assert_equal Errno::ECONNREFUSED, e.class
   end
 end
 
