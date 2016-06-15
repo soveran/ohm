dc1241e47b92841b7b8def03662ff7f137af7b4e
TheNotary <no@email.plz>
2016-06-15 15:36:42 -0700

    made assertion that works, fixed flaws, closer to final draft?

--- examples/rogue_associations.rb
+++ examples/rogue_associations.rb
@@ -12,27 +12,45 @@ def battleable
   attribute :mp,         lambda { |x| x.to_i }
   list      :inventory,  :Item
 
+  attribute :equipment_id
   reference :equipment,  :Loadout
 end
-
 
 # We define both a `Video` and `Audio` model, with a `list` of *comments*.
 class Rogue < Ohm::Model
   battleable # give Rogues all the attributes required to conduct battle
+
+  # instead of using Rogue.create, favor Rogue.spawn
+  def self.spawn(name: nil)
+    rogue = self.create(name: name)
+    rogue.equipment = Loadout.create
+    rogue.save
+    rogue
+  end
 end
 
 class Monster < Ohm::Model
   battleable # give monsters attributes required to conduct battle
+
+
 end
 
 class Loadout < Ohm::Model
+  attribute :head_id
   reference :head,       :Item
+  attribute :right_hand_id
   reference :right_hand, :Item
+  attribute :left_hand_id
   reference :left_hand,  :Item
+  attribute :body_id
   reference :body,       :Item
+  attribute :legs_id
   reference :legs,       :Item
+  attribute :feet_id
   reference :feet,       :Item
+  attribute :wrist_id
   reference :wrist,      :Item
+  attribute :neck_id
   reference :neck,       :Item
 end
 
@@ -41,27 +59,38 @@ class Item < Ohm::Model
   attribute :dmg,    lambda { |x| x.to_i }
   attribute :def,    lambda { |x| x.to_i }
   attribute :value,  lambda { |x| x.to_i }
+
+  def sell_to(target)
+    original_owner = "hmmm...."
+  end
 end
+
+
 
 
 # Now let's require the test framework we're going to use called
 # [cutest](http://github.com/djanowski/cutest)
 require "cutest"
 
+
 # And make sure that every run of our test suite has a clean Redis instance.
 prepare { Ohm.flush }
 
-test "all works" do
-  rogue = Rogue.create
+test "a rogue can be equipped with leggins" do
+  rogue = Rogue.spawn
 
-  # TODO: can this be initialized on creation of a Rogue?
-  rogue.equipment = Loadout.create
-
-  # TODO:
-  #  1) Can 'starting leggins' come automatic?
-  #  2) Is there a find_or_create method available so users don't wind up
-  #     making an ineffient qty of Items?
   rogue.equipment.legs = Item.create(name: "Starting Leggings", def: "1")
 
-  # assert_equal audio.comments.size, 1
+  # TODO: Should I do something to make saving a parent also save children?
+  rogue.save
+  rogue.equipment.save
+
+  rogue = Rogue[rogue.id] # lookup the rogue from the DB to make sure things are persistent
+  assert_equal rogue.equipment.legs.def, 1
+end
+
+test "a rogue can sell her gear" do
+  rogue = Rogue.spawn
+
+
 end
